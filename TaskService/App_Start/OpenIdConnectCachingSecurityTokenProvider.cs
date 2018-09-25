6bebb762dd1d08fa4048420dfc53487f76ed5b10
Alexander.Bartosh <Alexander.Bartosh@zylab.com>
2018-09-25 18:03:57 +0200

    Less code, much simpler code - the same behavior OpenIdConnectCachingSecurityTokenProvider

--- TaskService/App_Start/OpenIdConnectCachingSecurityTokenProvider.cs
+++ TaskService/App_Start/OpenIdConnectCachingSecurityTokenProvider.cs
@@ -3,8 +3,6 @@ using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Owin.Security.Jwt;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace TaskService.App_Start
 {
@@ -12,19 +10,11 @@ namespace TaskService.App_Start
     // the OpenID Connect metadata endpoint exposed by the STS by default.
     public class OpenIdConnectCachingSecurityTokenProvider : IIssuerSecurityKeyProvider
     {
-        public ConfigurationManager<OpenIdConnectConfiguration> _configManager;
-        private string _issuer;
-        private IEnumerable<SecurityKey> _keys;
-        private readonly string _metadataEndpoint;
-
-        private readonly ReaderWriterLockSlim _synclock = new ReaderWriterLockSlim();
-
+        private readonly ConfigurationManager<OpenIdConnectConfiguration> _configManager;
         public OpenIdConnectCachingSecurityTokenProvider(string metadataEndpoint)
         {
-            _metadataEndpoint = metadataEndpoint;
             _configManager = new ConfigurationManager<OpenIdConnectConfiguration>(metadataEndpoint, new OpenIdConnectConfigurationRetriever());
-
-            RetrieveMetadata();
+            _configManager.GetConfigurationAsync().Wait();
         }
 
         /// <summary>
@@ -33,22 +23,7 @@ namespace TaskService.App_Start
         /// <value>
         /// The issuer the credentials are for.
         /// </value>
-        public string Issuer
-        {
-            get
-            {
-                RetrieveMetadata();
-                _synclock.EnterReadLock();
-                try
-                {
-                    return _issuer;
-                }
-                finally
-                {
-                    _synclock.ExitReadLock();
-                }
-            }
-        }
+        public string Issuer => _configManager.GetConfigurationAsync().Result.Issuer;
 
         /// <summary>
         /// Gets all known security keys.
@@ -56,36 +31,6 @@ namespace TaskService.App_Start
         /// <value>
         /// All known security keys.
         /// </value>
-        public IEnumerable<SecurityKey> SecurityKeys
-        {
-            get
-            {
-                RetrieveMetadata();
-                _synclock.EnterReadLock();
-                try
-                {
-                    return _keys;
-                }
-                finally
-                {
-                    _synclock.ExitReadLock();
-                }
-            }
-        }
-
-        private void RetrieveMetadata()
-        {
-            _synclock.EnterWriteLock();
-            try
-            {
-                OpenIdConnectConfiguration config = Task.Run(_configManager.GetConfigurationAsync).Result;
-                _issuer = config.Issuer;
-                _keys = config.SigningKeys;
-            }
-            finally
-            {
-                _synclock.ExitWriteLock();
-            }
-        }
+        public IEnumerable<SecurityKey> SecurityKeys => _configManager.GetConfigurationAsync().Result.SigningKeys;
     }
 }
